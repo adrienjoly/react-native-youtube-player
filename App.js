e6927d4895358c90960507aaeffc8bb402f88469
Adrien Joly <adrien.joly@gmail.com>
2017-10-23 17:55:55 +0200

    also works on htc desire but video stops when switching apps

--- App.js
+++ App.js
@@ -4,27 +4,21 @@ import { StyleSheet, Text, View, WebView } from 'react-native';
 export default class App extends React.Component {
   render() {
     console.log('render!');
+    const uri = 'https://www.youtube.com/embed/ZZ5LpwO-An4?rel=0&autoplay=0&showinfo=0&controls=0';
     return (
-      /*
-      <View style={styles.container}>
-        <Text>Open up App.js to edit the app!</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <WebView
-          style={{flex:1}}
-          javaScriptEnabled={true}
-          source={{uri: 'https://www.youtube.com/embed/ZZ5LpwO-An4?rel=0&autoplay=0&showinfo=0&controls=0'}}
-          onReady={e => this.setState({ isReady: true })}
-          onChangeState={e => this.setState({ status: e.state })}
-          onChangeQuality={e => this.setState({ quality: e.quality })}
-          onError={e => console.log(e)}
-        />
-      </View>
-      */
-      <View style={styles.container}>
-        <Text>Open up App.js to edit the app!</Text>
-        <Text>Shake your phone or Cmd-M to open the developer menu.</Text>
-        <WebView style={styles.video} source={{ uri: 'https://www.youtube.com/embed/ZZ5LpwO-An4?rel=0&autoplay=0&showinfo=0&controls=0' }} />
-      </View>
+    <View style={styles.container}>
+      <Text>Open up App.js to edit the app!</Text>
+      <Text>Shake your phone or Cmd-M to open the developer menu.</Text>
+      <WebView
+        style={styles.video}
+        javaScriptEnabled={true}
+        source={{ uri }}
+        onReady={e => this.setState({ isReady: true })}
+        onChangeState={e => this.setState({ status: e.state })}
+        onChangeQuality={e => this.setState({ quality: e.quality })}
+        onError={e => console.log(e)}
+      />
+    </View>
     );
   }
 }
@@ -37,6 +31,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   video: {
+    //flex: 1,
     width: 100,
     height: 100,
     borderWidth: 0.5,
