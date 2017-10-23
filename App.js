1fcca835bc15a28a68ed46864b6f2a9b9462f322
Adrien Joly <adrien.joly@gmail.com>
2017-10-23 18:13:28 +0200

    keeps playing when screen turns off (https://stackoverflow.com/questions/36382935/how-to-prevent-react-native-android-webview-from-running-youtube-in-the-backgrou) …but not when switching to another app.

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, WebView } from 'react-native';
 export default class App extends React.Component {
   render() {
     console.log('render!');
-    const uri = 'https://www.youtube.com/embed/ZZ5LpwO-An4?rel=0&autoplay=0&showinfo=0&controls=0';
+    const uri = 'https://www.youtube.com/embed/ZZ5LpwO-An4?rel=0&autoplay=1&showinfo=0&controls=0';
     return (
     <View style={styles.container}>
       <Text>Open up App.js to edit the app!</Text>
@@ -12,6 +12,8 @@ export default class App extends React.Component {
       <WebView
         style={styles.video}
         javaScriptEnabled={true}
+        mediaPlaybackRequiresUserAction={false}
+        allowsInlineMediaPlayback={true}
         source={{ uri }}
         onReady={e => this.setState({ isReady: true })}
         onChangeState={e => this.setState({ status: e.state })}
